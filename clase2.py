a2b9f66239b31ff1226f8bc45c6fed46b9931364
--global <-global>
2025-05-22 18:42:40 -0600

    Actualizando los archivos para incluir todos los ejercicios completos

--- clase2.py
+++ clase2.py
@@ -1,3 +1,14 @@
+'''
+Clase:        Variables, tipos, entradas y salidas
+Tema:         Variables
+Ejercicio:    Uso de variables, entradas y salidas
+Descripción:  Un ejercicio para calcular propinas, otro para generar un correo segun un nombre dado y otro para dividir 2 numeros y establecer la cantidad de decimales que mostrara
+
+Autor:        Andres Arturo Marroquin Soto
+Fecha:        2025-05-16
+Estado:       [ Terminado ]
+'''
+
 #Ejercicio 1
 print("Ejercicio 1")
 cuenta = int(input("Cuenta total: $"))
@@ -16,8 +27,6 @@ elif len(full_name) == 4:
 else:
     print("No cumple con el formato")
 
-
-
 #Ejercicio 3
 print("Ejercicio 3")
 
@@ -26,28 +35,34 @@ try:
     b = int(input("Ingrese otro número entero: "))
     k = int(input("Ingrese el numero de decimales: "))
 
-    if k >= 0: 
+    if k > 0: 
         if a%b == 0:
-            resultado = str(a//b) + "." + "0"*k 
+            division = a//b
+            resultado = f"{division}." + "0" * k
             #resultado = str(a/b)+"0"*(k-1)
             
         elif a%b != 0:
             division = str(a/b) 
-            resultado = f"{division[0]}."
+            enteros = len(str(a//b))
+            resultado = ""
+            for i in range(enteros):
+                resultado += division[i]
 
-            for i in range(k):
+            for i in range(k+1):
                 try:    
-                    resultado += division[i+2]
+                    resultado += division[i+enteros]
                 except IndexError:
                     resultado += "0"*(k-i)
                     break
-        print(resultado)
+
     
     elif k == 0:
-        print(a//b)
-    
+        resultado = a//b
+
     elif k < 0:
-        print("Como va a tener decimales negativos pendejo")
+        print("No puedes poner un negativo")
+    
+    print(resultado)
         
 except ZeroDivisionError:
     print("No se puede dividir entre cero")
@@ -58,4 +73,28 @@ except ValueError:
 except TypeError:
     print("No puedes poner un decimal en k")
 
-    
+
+#Ejercicio 1.4.2 Suma de cadenas de texto
+
+a = "12345678901234567899"
+b = "98765432109876543215"
+
+a = a[::-1]
+b = b[::-1]
+c = 0
+resultado = ""
+
+
+for i,j in zip(a,b):
+    i = int(i)
+    j = int(j)
+    suma = i + j + c
+    if suma > 9:
+        c = 1
+        digito = str(suma-10)
+    else:
+        c = 0
+        digito = str(suma)
+    resultado = digito + resultado
+
+print(resultado)
--- clase3.py
+++ clase3.py
@@ -1,5 +1,16 @@
+'''
+Clase:        Bloque condicional
+Tema:         Operadores condicionales
+Ejercicio:    Uso de if, elif, else, and, or, not
+Descripción:  Ejercicios de verificar seguridad de contraseña y otro que identifica si un número es mágico.
+
+Autor:        Andres Arturo Marroquin Soto
+Fecha:        2025-05-16
+Estado:       [ Terminado ]
+'''
+
 # Ejercicio 1
-password = 'Poneme10'
+password = 'Pongame10porfa'
 upper = 0
 digit = 0 
 
@@ -14,11 +25,28 @@ if upper > 0 and digit > 0 and len(password) >= 8:
 else: 
     print("Contraseña no es segura")
 
-# Ejercicio 2
+#Ejercicio 2
+
+consumo = 150
+if consumo <= 100:
+    print("Sin impuesto")
+elif consumo > 100 and consumo <= 200:
+    print(f"Su impuesto a pagar es ${consumo*0.5}")
+elif consumo > 200:
+    print(f"Su impuesto a pagar es ${consumo*0.7}")
+
+# Ejercicio 3
 
 number = 21
-
 if number%7 == 0 and number%5 != 0:
     print("Es magico")
 else: 
     print("No es magico")
+
+# Ejercicio 4
+
+year = 2024
+if year%4 == 0 or (year%100 == 0 and year%400 == 0):
+    print("Es bisiesto")
+else:
+    print("No es bisiesto")
