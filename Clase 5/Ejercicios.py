54d1e7efe6a1c09cba5c731602df482f271dc21a
--global <-global>
2025-05-28 14:10:11 -0600

    Ejercicio  de clase4

---
+++ Clase 5/Ejercicios.py
@@ -0,0 +1,6 @@
+x = 10
+
+for i in range(x,0,-1):
+    print("*"*i)
+for i in range(0,x):
+    print("*"*i)
---
+++ clase4.py
@@ -0,0 +1,6 @@
+x = 10
+
+for i in range(x,0,-1):
+    print("*"*i, " "*2*(x-i), "*"*i)
+for i in range(1,x+1):
+    print("*"*i, " "*2*(x-i), "*"*i)   
