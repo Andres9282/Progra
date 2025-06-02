c92af8d13b3f6999239c38c2a86ea32ffd13db1b
--global <-global>
2025-06-02 14:19:23 -0600

    Cambios realizados en la Clase 6

--- Clase6/Ejercicio 6.3.1. Números líderes.py
+++ Clase6/Ejercicio 6.3.1. Números líderes.py
@@ -9,27 +9,14 @@ Fecha:        2025-05-31
 Estado:       [ Terminado ]
 '''
 
-lista = input("Ingrese una lista de números separados por espacios: ")
-
-lista = lista.split()
-lista_int = []
-for i in lista :
-    i = int(i)
-    lista_int.append(i)
-
-lista = lista_int
-    
+lista = input("Ingrese una lista de números separados por espacios: ").split()
+lista = list(map(int, lista))
+       
 lideres = []
 for i in range(len(lista)):
     lista2 = lista[i:]
     if lista[i-1] > max(lista2):
         lideres.append(lista[i-1])
 
-lideres_str = []
-for i in lideres:
-    i = str(i)
-    lideres_str.append(i)
-
-lideres = ' '.join(lideres_str)
-print(lideres)
-    
+lideres = list(map(str, lideres))
+print(*lideres)
